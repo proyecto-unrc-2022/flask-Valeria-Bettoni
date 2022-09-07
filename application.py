a1bb8a7c4c6bd95b7f48fc1401a54d6517932936
VALERIA <valebettoni.vb@gmail.com>
2022-09-07 20:30:58 -0300

    application

--- application.py
+++ application.py
@@ -17,5 +17,45 @@ def access_users(username):
         else:
             return Response(status=404)
 
+#New user scenario -----------------------------------
+@app.route("/users/newuser", methods=['POST'])
+def newuser():
+    if request.method == 'POST':
+        USERS.update(request.json)
+        return jsonify(USERS)
+    else:
+        return Response(status=404)
+
+#Update a user from USERS scenario --------------------
+@app.route("/users/update/<username>", methods=['PUT'])
+def updateuser(username):
+    if request.method == 'PUT':
+        USERS.update({username : request.json})
+        return jsonify(USERS.get(username))
+    else:
+        return Response(status=404)
+
+#Delete a existing user scenario -----------------------
+@app.route("/users/delete/<username>", methods=['DELETE'])
+def deleteuser(username):
+    if request.method == 'DELETE':
+        user_info = USERS.get(username)
+        USERS.pop(username)
+        return jsonify(USERS)
+    else:
+        return Response(status=404)
+
+#List all users escenario
+@app.route("/users/all/<allusers>", methods=['GET'])
+def all_users(allusers):
+    if request.method == 'GET':
+        return jsonify(allusers)
+    else:
+        return Response(status=404)
+
+
+
+
+
 if __name__ == "__main__":
     app.run()
